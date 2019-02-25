45bf69593465a1f6b97ba78c1538cba90f57b02b
eechava6 <eechava6@eafit.edu.co>
2019-02-25 17:43:27 -0500

    Vuetify added

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import VueI18n from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
+import Vuetify from 'vuetify'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/dashboard.css'
 import './assets/css/fontawesome.css'
 
+
 Vue.config.productionTip = false
 Vue.use(VueI18n)
-
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
